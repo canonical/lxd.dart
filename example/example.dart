2011e0d605bdd657c85a181825a5fa1adad4da2c
Robert Ancell <robert.ancell@canonical.com>
2021-08-26 15:25:00 +1200

    Make example list instances

--- example/example.dart
+++ example/example.dart
@@ -2,5 +2,12 @@ import 'package:lxd/lxd.dart';
 
 void main() async {
   var client = LxdClient();
+
+  var instances = await client.getInstances();
+  print('NAME STATE TYPE');
+  for (var instance in instances) {
+    print('${instance.name} ${instance.status} ${instance.type}');
+  }
+
   client.close();
 }
